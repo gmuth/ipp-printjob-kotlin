8fd802309087dd1e855ac9bdd9f3df40baf47a8f
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-20 11:21:56 +0200

    minor refactoring

--- src/main/kotlin/ipp/printJobWithStatusOnly.kt
+++ src/main/kotlin/ipp/printJobWithStatusOnly.kt
@@ -32,9 +32,9 @@ fun printJobWithStatusOnly(uri: URI, documentInputStream: InputStream) {
                 writeShort(value.length)
                 write(value.toByteArray(Charsets.US_ASCII))
             }
-            writeShort(0x0101) // ipp version
+            writeShort(0x0101) // ipp version 1.1
             writeShort(0x0002) // print job operation
-            writeInt(0x002A) // request id
+            writeInt(1) // request 1
             writeByte(0x01) // operation group tag
             writeAttribute(0x47, "attributes-charset", "us-ascii")
             writeAttribute(0x48, "attributes-natural-language", "en")
@@ -45,10 +45,9 @@ fun printJobWithStatusOnly(uri: URI, documentInputStream: InputStream) {
             close()
         }
         // get http status and ipp status
-        if (responseCode == 200) {
-            val dataInputStream = DataInputStream(inputStream)
-            println(String.format("ipp version %d.%s", dataInputStream.readByte(), dataInputStream.readByte()))
-            println(String.format("ipp status: %04X", dataInputStream.readShort()))
+        if (responseCode == 200) DataInputStream(inputStream).run {
+            println(String.format("version %d.%d", readByte(), readByte()))
+            println(String.format("status  %04X", readShort()))
         } else {
             throw IOException("post to $uri failed with http status $responseCode")
         }
